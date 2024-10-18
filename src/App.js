038ad9c2500932de387bf504dc62475cfb8b003b
Mohamed Abassia <mo3bassia@gmail.com>
2024-10-18 15:53:40 +0300

    add static part of jsx and styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
+import "./style.css";
+
 export default function App() {
   return (
-    <div>
+    <Container>
       <input type="text" />
-      <select>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="CAD">CAD</option>
-        <option value="INR">INR</option>
-      </select>
-      <select>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="CAD">CAD</option>
-        <option value="INR">INR</option>
-      </select>
-      <p>OUTPUT</p>
-    </div>
+      <div className="select-group">
+        <label htmlFor="from">from</label>
+        <select id="from">
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="INR">INR</option>
+        </select>
+        <label htmlFor="to">to</label>
+        <select id="to">
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="INR">INR</option>
+        </select>
+      </div>
+
+      <h2>OUTPUT</h2>
+    </Container>
   );
+}
+
+function Container({ children }) {
+  return <div className="container">{children}</div>;
 }
